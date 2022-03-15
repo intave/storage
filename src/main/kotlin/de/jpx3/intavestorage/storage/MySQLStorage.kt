72b289cbf67c6ba5cb59542283a537dd3c09173c
Lennox <lennox2508@gmail.com>
2022-03-15 21:42:11 +0100

    begin basic database implementation

---
+++ src/main/kotlin/de/jpx3/intavestorage/storage/MySQLStorage.kt
@@ -0,0 +1,41 @@
+package de.jpx3.intavestorage.storage
+
+import java.nio.ByteBuffer
+import java.sql.Connection
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+
+class MySQLStorage : CustomStorageGateway {
+    private val connection: Connection = TODO()
+
+    init {
+        prepareTables()
+    }
+
+    private fun prepareTables() {
+        connection.autoCommit = false
+        connection.prepareStatement("CREATE TABLE IF NOT EXISTS intave_storage (id VARCHAR(36) NOT NULL, bytes MEDIUMBLOB NOT NULL, time LONG)")
+        connection.commit()
+    }
+
+    override fun clearEntriesOlderThan(value: Long, unit: TimeUnit) {
+    }
+
+    override fun requestStorage(uuid: UUID, consumer: Consumer<ByteBuffer>) {
+        // Query with the given uuid
+        val preparedStatement = connection.prepareStatement("SELECT * FROM intave_storage WHERE id = ?")
+        preparedStatement.setString(1, uuid.toString())
+        val resultSet = preparedStatement.executeQuery()
+        if (!resultSet.next()) {
+            // Accept with empty array as no entry could be found
+            consumer.accept(ByteBuffer.wrap(ByteArray(0)))
+            return
+        }
+        // Accept with the bytes of the entry
+        consumer.accept(ByteBuffer.wrap(resultSet.getBytes("bytes")))
+    }
+
+    override fun saveStorage(uuid: UUID, buffer: ByteBuffer) {
+    }
+}
--- src/main/kotlin/de/jpx3/intavestorage/storage/StorageOption.kt
+++ src/main/kotlin/de/jpx3/intavestorage/storage/StorageOption.kt
@@ -8,7 +8,8 @@ enum class StorageOption(
     private val resolve: (ConfigurationSection) -> StorageGateway
 ) {
     NONE({ EmptyStorageGateway() }),
-    FILE({ config -> FileStorage(config) })
+    FILE({ config -> FileStorage(config) }),
+    MY_SQL({ MySQLStorage() })
 
     ;
 
