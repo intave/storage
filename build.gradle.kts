f206a6e3b3047287fe5e8bc72caa3d3f7ceeabde
Trattue <63713970+Trattue@users.noreply.github.com>
2022-03-16 14:23:29 +0100

    groovy -> kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "de.jpx3"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+    compileOnly(fileTree("libs/") { include("*.jar") })
+    implementation("org.postgresql:postgresql:42.3.3")
+}
